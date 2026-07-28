e0d9d0375c01922384e2fc44550f1d2ba09e0735
namanduaa <duanaman41@gmail.com>
2026-07-28 18:09:02 +0530

    Add guess the number game implementation

---
+++ guess_the_number.py
@@ -0,0 +1,16 @@
+import random
+
+target = random.randint(1, 100)
+
+while True:
+    try:
+        guess = int(input("guess the number: "))
+        if guess < target:
+            print("low guess")
+        elif guess > target:
+            print("high guess")
+        elif guess == target:
+            print("congratulations")
+            break
+    except ValueError:
+             print("invalid number")
